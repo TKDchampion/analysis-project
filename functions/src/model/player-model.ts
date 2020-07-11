64a88e0dea7afe725fc98ce97b5b97c05a707542
Giant <giant@Giantde-MacBook-Pro.local>
2020-07-11 19:47:02 +0800

    add verify

--- functions/src/model/player-model.ts
+++ functions/src/model/player-model.ts
@@ -1,26 +1,17 @@
 import { db } from "../detabase/setting";
 import { dataBase } from "../detabase/db-interface";
 import { verify } from "./verify-model";
-import { ErrorContent } from "../view-model/error-viewmodel";
 
 class PlayerModel {
     public getPlayersList(req: any) {
+
         const reference = db.collection('playersList').doc('list');
-        const asyncData = dataBase.get({ reference: reference }, this.verifyFtn(req));
+        const formatResultFn = (result: any) => { return result.data() };
+        const asyncData = dataBase.get({ reference: reference }, verify.verifyUser(req, formatResultFn));
         return asyncData;
     }
 
-    private verifyFtn(req: any){
-        const formatResultFn = (result: any) => {
-            const userVerify = verify.verifyToken(req);
-            if (userVerify.counts > 0) {
-                return result.data()
-                // return userVerify
-            }
-            return { message: 'user unauthorized', errorStatus: 401 } as ErrorContent;
-        }
-        return formatResultFn;
-    }
+    
 }
 
 export const playerModel = new PlayerModel();
--- functions/src/model/verify-model.ts
+++ functions/src/model/verify-model.ts
@@ -1,3 +1,5 @@
+import { ErrorContent } from "../view-model/error-viewmodel";
+
 const jwt = require('jsonwebtoken');
 
 class VerifyModel {
@@ -5,10 +7,23 @@ class VerifyModel {
     public account: string = '';
     public password: string = '';
 
-    public verifyToken(req: any) {
-        const secretKey = 'shhhhh';
-        const token = jwt.verify(req.header('Authorization').replace('Bearer ', ''), secretKey);
-        return token;
+    public verifyUser(req: any, ftn: Function) {
+        return jwt.verify(req.header('Authorization').replace('Bearer ', ''), 'shhhhh',
+            (error: { message: any; }, decoded: any) => {
+                if (error) {
+                    return this.formatResultErrorFn;
+                }
+
+                if (decoded.account) {
+                    return ftn
+                } else {
+                    return this.formatResultErrorFn;
+                }
+            })
+    }
+
+    private formatResultErrorFn = (result: any) => {
+        return { message: 'user unauthorized', errorStatus: 401 } as ErrorContent;
     }
 }
 
